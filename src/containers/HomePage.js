97aee37aa95d3cf984b0e65e3097e3edc9a8b83a
ericguerrero-RL <86261083+ericguerrero-RL@users.noreply.github.com>
2021-06-28 15:30:44 -0700

    Change to view details for button

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -78,7 +78,7 @@ function renderLander() {
                 <Card.Title style={{textAlign:'center'}}>{card[0]}</Card.Title>
                 <Card.Img variant="top" src={card[2]} />
                 <div class="text-center">
-                <Button href={"/listing/"+ card[1]} variant="primary">listingID: {card[1]}</Button>
+                <Button href={"/listing/"+ card[1]} variant="primary">View Details</Button>
                 </div>
             </Card.Body>
             </Card>
